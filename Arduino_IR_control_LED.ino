d08b95f1c3da4033352e0d576cfa8271548b2227
Noahcpp <30606176+Noahcpp@users.noreply.github.com>
2017-07-31 15:04:33 -0600

    Add files via upload

---
+++ Arduino_IR_control_LED.ino
@@ -0,0 +1,40 @@
+// Written by: Noahcpp
+// This code is for controlling an LED with IR remote control
+
+#include <IRremote.h>      //must copy IRremote library to arduino libraries
+int on = 0; //Put the button's HEX value here
+int off = 0; //Put the button's HEX value here
+
+int RECV_PIN = 2;       //IR receiver pin
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+int LEDpin = 13;   //LED pin
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+
+  pinMode(LEDpin, OUTPUT);
+}
+
+void loop() 
+{
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+
+    if (results.value == on)   //Check value
+    {
+      digitalWrite(LEDpin, HIGH);
+    }
+
+    else if (results.value == off)
+    {
+      digitalWrite(LEDpin, LOW);
+    }
+  }
+}
